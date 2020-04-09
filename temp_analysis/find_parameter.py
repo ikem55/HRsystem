e6200be62cd1346c1fd2310966ba4bf9ef038aa4
ikem55 <ikem1984@gmail.com>
2020-04-09 18:27:49 +0900

    bug fix

--- temp_analysis/find_parameter.py
+++ temp_analysis/find_parameter.py
@@ -53,8 +53,8 @@ print("------ check ------")
 print(df.shape)
 
 iter_range = 5
-score_rate = range(0, 51, iter_range)
-v3_rate = range(0, 41, iter_range)
+score_rate = range(0, 1, iter_range)
+v3_rate = range(0, 1, iter_range)
 win_rate = range(0, 101, iter_range)
 jiku_rate = range(0, 101, iter_range)
 ana_rate = range(0, 101, iter_range)
